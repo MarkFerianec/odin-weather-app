5b63cedbd16e79be672bea684131b38eb8ff3cd1
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-12-29 14:24:43 -0700

    Modify script
    Added functions that log some weather data to console.

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
+let weatherData = undefined;
+
 async function fetchForecastFromWeatherAPI(location) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=7c8af27164954524aab224342232312&q=${location}&days=3`,
     { mode: 'cors' }
   );
-  const weatherData = await response.json();
+  weatherData = await response.json();
   console.log(weatherData);
+  //   return weatherData;
 }
 
-fetchForecastFromWeatherAPI('London');
+// fetchForecastFromWeatherAPI('London');
+
+function returnCurrentTemperature() {
+  //   weatherData = await fetchForecastFromWeatherAPI('London');
+  console.log(weatherData.current.temp_c);
+  console.log(weatherData.current.temp_f);
+}
+
+function returnForecastTemperatures() {
+  console.log(weatherData.forecast.forecastday[0].day.maxtemp_c);
+  console.log(weatherData.forecast.forecastday[0].day.mintemp_c);
+  console.log(weatherData.forecast.forecastday[0].day.maxtemp_f);
+  console.log(weatherData.forecast.forecastday[0].day.mintemp_f);
+
+  //   console.log(weatherData.forecast.forecastday[1].day.maxtemp_c);
+  //   console.log(weatherData.forecast.forecastday[1].day.mintemp_c);
+  //   console.log(weatherData.forecast.forecastday[1].day.maxtemp_f);
+  //   console.log(weatherData.forecast.forecastday[1].day.mintemp_f);
+
+  //   console.log(weatherData.forecast.forecastday[2].day.maxtemp_c);
+  //   console.log(weatherData.forecast.forecastday[2].day.mintemp_c);
+  //   console.log(weatherData.forecast.forecastday[2].day.maxtemp_f);
+  //   console.log(weatherData.forecast.forecastday[2].day.mintemp_f);
+}
+
+// function returnThreeDayForecastTemperatures() {}
+
+function logTemperatures() {
+  returnCurrentTemperature();
+  returnForecastTemperatures();
+}
+
+function fetchForecastFromWeatherAPIAndLogTemperatures() {
+  fetchForecastFromWeatherAPI('London').then(function () {
+    logTemperatures();
+  });
+}
+
+fetchForecastFromWeatherAPIAndLogTemperatures();
+
+// processForecastAndReturnCurrentTemp();
+
+// fetchForecastFromWeatherAPI().then(processForecastAndReturnCurrentTemp());
